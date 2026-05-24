97c1a8c9f20f77fc4abd897e19c2b5d0f0852386
Raphael David <raphaelruizd@gmail.com>
2026-05-24 22:02:33 +1000

    Implement staleTime caching on queried data

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,10 +2,19 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { useState } from "react"
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+    // Implement a singleton pattern for QueryClient to ensure it's not recreated on every render
+    const [queryClient] = useState(() => new QueryClient({
+        defaultOptions: {
+            queries: {
+                staleTime: 1000 * 60 * 5, // Cache data for 5 minutes
+            }
+        }
+    }));
     return (
         <QueryClientProvider client={queryClient}>
             {children}
